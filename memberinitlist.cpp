271ddfb54db91cae3202e4aa203a83d82fb51cc2
nisrinasals <nisrinahikmawan@gmail.com>
2024-05-20 14:38:00 +0700

    membuat akses modifier public dan constructor mahasiswa

--- memberinitlist.cpp
+++ memberinitlist.cpp
@@ -8,4 +8,9 @@ private:
     string nama;
     float nilai;
 
+public:
+    mahasiswa(int pId, string pNama, float pNilai):id (pId), nama(pNama), nilai(pNilai)
+    {//Member Initialization List
+        //Definisi
+    }
 };
